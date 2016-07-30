ae9d95dd6e135ef41d8c6c96ef1fc064865a01cd
shash7 <shashwat.amin@yahoo.com>
2016-07-30 23:08:06 +1000

    Fixed another bug

--- deployment/lib/passport.js
+++ deployment/lib/passport.js
@@ -44,7 +44,7 @@ module.exports = function(passport, app) {
 		process.nextTick(function() {
 
 			// find the user in the database based on their facebook id
-			db.end_user.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+			db.end_user.findOne({ 'facebook_id' : profile.id }, function(err, user) {
 
 			// if there is an error, stop everything and return that
 			// ie an error connecting to the database
