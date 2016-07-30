01c53bf650e13d59535a0ecea3da63560b6653c9
shash7 <shashwat.amin@yahoo.com>
2016-07-30 19:32:21 +1000

    Setuped the ctps

--- deployment/main.js
+++ deployment/main.js
@@ -13,9 +13,9 @@
 	
 	module.exports = function(cms, app, db) {
 
-		var obj = {
+		var goal = {
 
-			name      : 'Goal',   // Will be used for creating the database
+			name      : 'goal',   // Will be used for creating the database
 			plural    : 'Goals',  // Will be used in the backend UI
 			slug      : '/goals', // Will be used in creating urls
 			navName   : 'Goals',   // Optional; Will be used for the menu name
@@ -23,7 +23,6 @@
 			schema : {
  				title : {
  					type    : String,
- 					default : 'Post title',
  					fieldType : 'textarea'
  				},
  				description : {
@@ -58,7 +57,72 @@
 
 		};
 
-		cms.defineType(obj);
+		var end_user = {
+
+			name      : 'end_user',   // Will be used for creating the database
+			plural    : 'End User',  // Will be used in the backend UI
+			slug      : '/end-user', // Will be used in creating urls
+			navName   : 'End-user',   // Optional; Will be used for the menu name
+
+			schema : {
+ 				name : {
+ 					type    : String,
+ 					fieldType : 'text'
+ 				},
+ 				password : {
+ 					type : String,
+ 					default : '',
+ 					fieldType : 'text'
+ 				},
+ 				date_created : {
+ 					type : Date,
+ 					default : new Date(),
+ 					fieldType : 'text'
+ 				},
+ 				email : {
+ 					type : String,
+ 					fieldType : 'text'
+ 				},
+ 				goals_created : {
+ 					type : Array,
+ 					fieldType : 'text'
+ 				},
+ 				goals_completed : {
+ 					type : String,
+ 					fieldType : 'text'
+ 				},
+ 				bounty : {
+ 					type : Number,
+ 					fieldType : 'text'
+  			}
+ 			},
+
+ 			archiveTemplate : 'index'
+
+		};
+
+
+		var goal_comment = {
+
+			name      : 'goal_comment',   // Will be used for creating the database
+			plural    : 'Goal comments',  // Will be used in the backend UI
+			slug      : '/goal-comment', // Will be used in creating urls
+			navName   : 'goal-comment',   // Optional; Will be used for the menu name
+
+			schema : {
+ 				comment : {
+ 					type      :  Array,
+ 					fieldType : 'text'
+ 				}
+ 			},
+
+ 			archiveTemplate : 'index'
+
+		};
+
+		cms.defineType(end_user);
+		cms.defineType(goal);
+		cms.defineType(goal_comment);
 	}
 
 })();
---
+++ init.bat
@@ -0,0 +1,6 @@
+
+set NODE_ENV=development
+
+start cmd /k gulp
+
+nodemon -e html,js index.js
