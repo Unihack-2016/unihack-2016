e0153d12bb306b07872e15e18b4ddce886ac96b8
shash7 <shashwat.amin@yahoo.com>
2016-07-30 19:12:01 +1000

    Merged changes

--- deployment/main.js
+++ deployment/main.js
@@ -15,16 +15,39 @@
 
 		var obj = {
 
-			name      : 'Picture',   // Will be used for creating the database
-			plural    : 'Pictures',  // Will be used in the backend UI
-			slug      : '/pictures', // Will be used in creating urls
-			navName   : 'Pictures',   // Optional; Will be used for the menu name
+			name      : 'Goal',   // Will be used for creating the database
+			plural    : 'Goals',  // Will be used in the backend UI
+			slug      : '/goals', // Will be used in creating urls
+			navName   : 'Goals',   // Optional; Will be used for the menu name
 
 			schema : {
  				title : {
  					type    : String,
  					default : 'Post title',
  					fieldType : 'textarea'
+ 				},
+ 				description : {
+ 					type : String,
+ 					default : '',
+ 					fieldType : 'textarea'
+ 				},
+ 				date_created : {
+ 					type : Date,
+ 					default : new Date(),
+ 					fieldType : 'text'
+ 				},
+ 				date_closing : {
+ 					type : Date,
+ 					fieldType : 'text'
+ 				},
+ 				status : {
+ 					type : Boolean,
+ 					default : 1,
+ 					fieldType : 'text'
+ 				},
+ 				comment_id : {
+ 					type : String,
+ 					fieldType : 'text'
  				},
  				author : {
  					type : String
